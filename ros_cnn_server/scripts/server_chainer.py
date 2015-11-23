6ccf4709e1f6411e907b08ee4d9154a47c4927d5
kazuto1011 <k_nakashima@irvs.ait.kyushu-u.ac.jp>
2015-11-23 23:54:52 +0900

    added a chainer version

--- ros_cnn_server/scripts/server_chainer.py
+++ ros_cnn_server/scripts/server_chainer.py
@@ -1,11 +1,14 @@
 import numpy as np
-import cv2
 import rospy as rp
+import cv2
+import time
 from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
-import caffe
-import time
 from config import config
+
+import chainer
+import chainer.functions as F
+from chainer.functions import caffe
 
 __author__ = 'kazuto1011'
 
@@ -15,29 +18,38 @@ TOPIC_NAME = "/camera/image/compressed"
 class CNNClassifier:
     def __init__(self):
         rp.loginfo("Initialization")
+        cv2.namedWindow("rgb", cv2.CV_WINDOW_AUTOSIZE)
 
         self.bridge = CvBridge()
         self.rgb_subscriber = rp.Subscriber(TOPIC_NAME, CompressedImage, self.classify, queue_size=1)
 
-        cv2.namedWindow("rgb", cv2.CV_WINDOW_AUTOSIZE)
-
         # initialize a classifier
-        caffe.set_device(config.gpuNum)
-        caffe.set_mode_gpu()
-        self.classifier = caffe.Classifier(config.path.caffe.prototxt, config.path.caffe.caffemodel)
+        self.caffe_net = caffe.CaffeFunction(config.path.caffe.caffemodel)
 
         # load synset_words
         self.categories = np.loadtxt(config.path.caffe.synset_words, str, delimiter="\t")
+
+    def predict(self, x):
+        y, = self.caffe_net(inputs={'data': x}, outputs=['fc8'], train=False)
+        return F.softmax(y)
 
     def classify(self, image):
         # convert CompressedImage to numpy array
         np_arr = np.fromstring(image.data, np.uint8)
         rgb_image = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
 
+        im_size = 224
+        rgb_image = cv2.resize(rgb_image, (im_size, im_size))
+
+        batch = np.zeros((1, 3, im_size, im_size), dtype='float32')
+        batch[0] = rgb_image.transpose(2, 0, 1)
+
+        x = chainer.Variable(batch, volatile=True)
+
         # prediction
         start = time.time()
-        predictions = self.classifier.predict([rgb_image], oversample=False)
-        index = np.argmax(predictions)
+        p_dist = self.predict(x)
+        index = np.argmax(p_dist.data)
         rp.loginfo("%.2f s. " % (time.time() - start) + self.categories[index])
 
         cv2.imshow("rgb", rgb_image)
